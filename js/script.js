fbbfdec6a06338a1cf735b1868644802a8fb81da
Flatiron School <github email address>
2016-12-08 15:38:33 -0500

    done

---
+++ js/script.js
@@ -0,0 +1,92 @@
+// Alternatively
+// function GithubInteractor(token) {
+//   // we know it's a constructor bc the Github is capitalized
+//   this.token = token
+// }
+
+class GithubInteractor {
+
+  constructor(token) {
+    this.token = token
+  }
+
+
+  // call function createIssue
+  // post request  to github endpoint
+  // if else -> if error console.log("Post error: error_name")
+
+
+}
+
+
+function submitForm(event) {
+  event.preventDefault();
+  // $('#submit').click(function(event) {
+    var title = $("#title").val()
+    var body = $("#body").val()
+    var repoName = $("#repoName").val()
+    var repoOwner = $("#repoOwner").val()
+
+    createIssue(repoName, repoOwner, title, body)
+  // }
+    // debugger;
+
+  // };
+}
+
+
+function createIssue(repoName, repoOwner, title, body) {
+      var token = "2aa6a704ae1b85b6e20af38a167c52e9c4aea56b"
+
+      // debugger;
+
+      var url = `https://api.github.com/repos/${repoOwner}/${repoName}/issues`
+
+      var info = {
+        title: `${title}`,
+        body: `${body}`
+      };
+
+          $.ajax({
+                type: "POST",
+                url: url,
+                dataType: "json",
+                data: JSON.stringify(info),
+                success: function(response){
+                  // debugger;
+                  handleResponse(response)
+                },
+                error: function(response) {
+                  debugger
+                  handleError()
+                },
+                headers: {
+                Authorization: `token ${token}`
+              }
+            })
+
+            // or alternatively
+            // .fail(function(){
+            //   handleError()
+            // }).done(function() {
+            //   handleResponse()
+            // })
+
+    }
+
+function handleResponse(data) {
+
+  $('#issue').text(data.title)
+
+}
+
+
+function handleError(jqXHR, textStatus, errorThrown) {
+  console.log("Post error: " + errorThrown)
+}
+//
+
+$(document).ready(function(){
+  $('form').on('submit', submitForm);
+  // submitForm() would run immediately when the page is rendered
+});
